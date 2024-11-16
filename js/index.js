45d3493a09e026eaa5b385eb061d75a5151a4399
yuukigg065 <yuukigg065@gmail.com>
2024-11-17 00:49:13 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Chinta Amelia Saputri'.split('')
+const text = 'Chinta Amelia ^,^'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
